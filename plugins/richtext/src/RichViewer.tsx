66ee7a397f1a33af91b4839cab6254b8f4f6944f
yunkai <hanguang.mhg@alibaba-inc.com>
2023-07-17 14:49:56 +0800

    update

--- plugins/richtext/src/RichViewer.tsx
+++ plugins/richtext/src/RichViewer.tsx
@@ -1,8 +1,47 @@
 import React from "react";
-import { BaseViewer } from 'form-driver';
+import { Viewer, ViewerState } from 'form-driver';
+import './RichViewer.less'
 
-export class RichViewer extends BaseViewer {
+interface State extends ViewerState {
+  isShow: boolean,
+}
+
+export class RichViewer extends Viewer<State> {
+  private options: any;
+
+  constructor(p: any) {
+    super(p);
+
+    this.options = Object.assign({
+      srcFormat: src => src
+    }, this.props.schema.options)
+    console.log('this.options', this.options)
+    console.log('this.getValue()', this.getValue())
+
+    this.state = {
+      ctrlVersion: 1,
+      noValidate: false,
+      isShow: false,
+    };
+  }
+
   element() {
-    return <div className={'ql-editor'} style={{ padding: 0 }} dangerouslySetInnerHTML={{ __html: this.getValue() }} />
+    const d = this.getValue()
+    const { isShow } = this.state
+    if (d.more) {
+      return <div className="form-driver-plugin-richtext-viewer-wrap">
+        <div className={'ql-editor'} style={{ padding: 0 }} dangerouslySetInnerHTML={{ __html: d.HTML }} />
+        {isShow ? <div className={'ql-editor'} style={{ padding: 0 }} dangerouslySetInnerHTML={{ __html: d.HTML2 }} /> : null}
+        <div className="richtext-viewer-more">
+          <span onClick={() => {
+            this.setState({ isShow: !isShow })
+          }}>{isShow ? '收起' : '展开'}{isShow ? <i className="u-arrow u-arrow-up">
+          </i> : <i className="u-arrow u-arrow-down ">
+          </i>}</span>
+        </div>
+      </div>
+    } else {
+      return <div className={'ql-editor'} style={{ padding: 0 }} dangerouslySetInnerHTML={{ __html: this.options.srcFormat(this.getValue()) }} />
+    }
   }
 }
