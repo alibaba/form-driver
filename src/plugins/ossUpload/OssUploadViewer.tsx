491a4c9d257e55f96e190fdd5136f4f111cd4ad9
yunkai <hanguang.mhg@alibaba-inc.com>
2024-05-15 18:52:51 +0800

    update

--- src/plugins/ossUpload/OssUploadViewer.tsx
+++ src/plugins/ossUpload/OssUploadViewer.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { BaseViewer } from "../../index";
 
+// 字节友好展示
+export function bytesToSize(bytes, fixNum = 1) {
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  var i = Math.floor(Math.log(bytes) / Math.log(1024));
+  if (sizes[i]) {
+    return (bytes / Math.pow(1024, i)).toFixed(fixNum) + ' ' + sizes[i];
+  } else {
+    return ''
+  }
+}
+
 class OssUploadViewer extends BaseViewer {
   [x: string]: any;
   element() {
@@ -8,7 +19,7 @@ class OssUploadViewer extends BaseViewer {
     return <div>
       {
         arr.map(i => {
-          return <div key={i.uid}><a target='_blank' href={i.url}>{i.name}</a></div>
+          return <div key={i.uid}><a target='_blank' href={i.url}>{i.name}</a>  {i.size ? <span> {bytesToSize(i.size)}</span> : ""} </div>
         })
       }
     </div>
